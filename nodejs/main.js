f90bc375c65f3d901067e971eec31f6c49d62a14
rasauq1122 <rasauq1122@gmail.com>
2020-06-01 01:49:11 +0900

    404 error 구현

--- nodejs/main.js
+++ nodejs/main.js
@@ -5,18 +5,11 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var querydata = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
     var id = querydata.id;
-    var body = "";
-
-    if(_url == '/'){
-        id = 0;
-    }
-    if(_url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
     
+    if (id === undefined) id = 0;
+
     var sub = "";
     var file = "index";
     if (1 <= id && id <= 4) {
@@ -24,27 +17,34 @@ var app = http.createServer(function(request,response){
         file = id;
     }
 
-    fs.readFile(`data/${file}`, "utf8", function(err, body){
-        var tpl = 
-        `<!DOCTYPE html>
-        <html>
-            <head>
-                <title>애국가 닷컴${sub}</title>
-                <meta charset="utf-8" />
-            </head>
-            <body>
-                <h1> <a href="/">애국가 닷컴</a> </h1>
-                <ol>
-                    <li> <a href="/?id=1">1절</a></li>
-                    <li> <a href="/?id=2">2절</a></li>
-                    <li> <a href="/?id=3">3절</a></li>
-                    <li> <a href="/?id=4">4절</a></li>
-                </ol>
-                ${body}
-            </body>
-        </html>`;
-        response.writeHead(200);
-        response.end(tpl);
-    })
+    if (pathname === '/') {
+        fs.readFile(`data/${file}`, "utf8", function(err, body){
+            var tpl = 
+            `<!DOCTYPE html>
+            <html>
+                <head>
+                    <title>애국가 닷컴${sub}</title>
+                    <meta charset="utf-8" />
+                </head>
+                <body>
+                    <h1> <a href="/">애국가 닷컴</a> </h1>
+                    <ol>
+                        <li> <a href="/?id=1">1절</a></li>
+                        <li> <a href="/?id=2">2절</a></li>
+                        <li> <a href="/?id=3">3절</a></li>
+                        <li> <a href="/?id=4">4절</a></li>
+                    </ol>
+                    ${body}
+                </body>
+            </html>`;
+            response.writeHead(200);
+            response.end(tpl);
+        })
+    }
+    else {
+        response.writeHead(404);
+        response.end('not found');
+    }
 });
+
 app.listen(3000);
